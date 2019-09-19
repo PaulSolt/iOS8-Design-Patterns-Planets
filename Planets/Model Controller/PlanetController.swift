76e004f1b4db68cdd2730150af1958d1235b218b
Paul Solt <PaulSolt@gmail.com>
2019-09-19 13:10:10 -0400

    Refactored PlanetController to provide a better API for the planet access.

--- Planets/Model Controller/PlanetController.swift
+++ Planets/Model Controller/PlanetController.swift
@@ -20,26 +20,33 @@ import Foundation
 
 
 class PlanetController {
-    var planetsWithoutPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        ]
     
-    var planetsWithPluto = [
-        Planet(name: "Mercury", imageName: "mercury"),
-        Planet(name: "Venus", imageName: "venus"),
-        Planet(name: "Earth", imageName: "earth"),
-        Planet(name: "Mars", imageName: "mars"),
-        Planet(name: "Jupiter", imageName: "jupiter"),
-        Planet(name: "Saturn", imageName: "saturn"),
-        Planet(name: "Uranus", imageName: "uranus"),
-        Planet(name: "Neptune", imageName: "neptune"),
-        Planet(name: "Pluto", imageName: "pluto"),
+    var planets: [Planet] {
+        var planetArray = [
+            Planet(name: "Mercury", imageName: "mercury"),
+            Planet(name: "Venus", imageName: "venus"),
+            Planet(name: "Earth", imageName: "earth"),
+            Planet(name: "Mars", imageName: "mars"),
+            Planet(name: "Jupiter", imageName: "jupiter"),
+            Planet(name: "Saturn", imageName: "saturn"),
+            Planet(name: "Uranus", imageName: "uranus"),
+            Planet(name: "Neptune", imageName: "neptune"),
         ]
+        
+        if shouldShowPluto {
+            planetArray.append(Planet(name: "Pluto", imageName: "pluto"))
+        }
+        
+        return planetArray
+    }
+
+    // function or var { }
+//    func shouldShowPluto() -> Bool {
+//        return UserDefaults.standard.bool(forKey: .shouldShowPlutoKey)
+//    }
+
+    var shouldShowPluto: Bool {
+        return UserDefaults.standard.bool(forKey: .shouldShowPlutoKey)
+    }
+    
 }
